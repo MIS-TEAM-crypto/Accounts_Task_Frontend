edd19d3d9e24f32b217ea9deba36002db7f42efe
MIS-TEAM-crypto <mis@hemrajgroup.co.in>
2026-01-20 15:12:52 +0530

    Re-added missing file

--- user-login.js
+++ user-login.js
@@ -25,7 +25,7 @@ const demoUsers = [
   { username: 'RISHI', password: 'R1234', fullName: 'Rishi Gupta' },
   { username: 'KANCHAN', password: 'K1234', fullName: 'Kanchan Kumar Saha' },
   { username: 'DEBASISH', password: 'D1234', fullName: 'Debasish Shyam' },
-  { username: 'ARUP', password: 'A1234', fullName: 'Arup Bhattarcharya' },
+  { username: 'ARUP', password: 'A1234', fullName: 'Arup Bhattacharya' },
   { username: 'PRADIP', password: 'P1234', fullName: 'Pradip Das' },
   { username: 'SAMIRON', password: 'S1234', fullName: 'Samiron Mondal' },
   { username: 'SAMIR', password: 'S1234', fullName: 'Samir Majhi' },
